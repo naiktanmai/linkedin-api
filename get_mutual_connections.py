6f7db3f76eeaad84b8b3600384e67504b4a75bbe
naiktanmai <tanmai.d.naik@gmail.com>
2020-03-11 17:04:12 +0530

    CHORE(added main back)

--- get_mutual_connections.py
+++ get_mutual_connections.py
@@ -18,7 +18,7 @@ class MutualConnections():
         else:
             data = self.api.get_profile_connections(urn_id)
         df = pd.DataFrame(data)
-        df.columns = ["distance", "profile name",  "urn"]
+        df.columns = ["distance", "profile name", "urn"]
         return df
 
     def get_my_urn_id(self):
@@ -32,29 +32,28 @@ class MutualConnections():
     def get_first_two_connections(self, company_urn_id=None):
         return self.api.search_people(network_depth=["F", "S"], current_company=[company_urn_id], limit=2)
 
+
 if __name__ == 'main':
-    pass
+    conn_inst = MutualConnections(user=config["user"], pwd=config["pwd"], urn=config["urn"])
 
-conn_inst = MutualConnections(user=config["user"], pwd=config["pwd"], urn=config["urn"])
+    my_urn_id = conn_inst.get_my_urn_id()
+    my_connections_df = conn_inst.get_connections(file_name='my_connections.json', urn_id=my_urn_id)
 
-my_urn_id = conn_inst.get_my_urn_id()
-my_connections_df = conn_inst.get_connections(file_name='my_connections.json', urn_id=my_urn_id)
+    output_df = pd.DataFrame()
 
-output_df = pd.DataFrame()
+    for company in companies:
+        _company_df = pd.DataFrame(columns=my_connections_df.columns)
+        company_urn = conn_inst.get_company_urn(company_name=company)
+        main_connections = conn_inst.get_first_two_connections(company_urn_id=company_urn)
+        for connection in main_connections:
+            sub_connection_df = conn_inst.get_connections(urn_id=connection['urn_id'])
 
-for company in companies:
-    _company_df = pd.DataFrame(columns=my_connections_df.columns)
-    company_urn = conn_inst.get_company_urn(company_name=company)
-    main_connections = conn_inst.get_first_two_connections(company_urn_id=company_urn)
-    for connection in main_connections:
-        sub_connection_df = conn_inst.get_connections(urn_id=connection['urn_id'])
+            _company_df = pd.merge(my_connections_df, sub_connection_df, on='urn', how="inner", suffixes=('', '_y'))
+            _company_df.drop(_company_df.filter(regex='_y').columns, axis=1, inplace=True)
 
-        _company_df = pd.merge(my_connections_df, sub_connection_df, on='urn', how="inner", suffixes=('', '_y'))
-        _company_df.drop(_company_df.filter(regex='_y').columns, axis=1, inplace=True)
+            _company_df["Company"] = company
+            _company_df["Primary connection"] = connection['public_id']
 
-        _company_df["Company"] = company
-        _company_df["Primary connection"] = connection['public_id']
+            output_df = output_df.append(_company_df, sort=False)
 
-        output_df = output_df.append(_company_df, sort=False)
-
-output_df.to_csv('output_df.csv')
+    output_df.to_csv('output_df.csv')
